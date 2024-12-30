4f9f4e517e593c3856f3d80e9c654f872c7f7ebf
Zulhilmi DN <zulhilmidharma@gmail.com>
2024-12-30 07:11:22 +0700

    Initial Commit

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  assetPrefix: "./",
+  output: "export", // Enables static HTML export
+  images: {
+    unoptimized: true, // Required for static export
+  },
+};
+
 export default nextConfig;
